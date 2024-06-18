df8ec770445b2f6eb8a159f03679dd90e3592530
unknown <nickives@live.com>
2024-06-17 22:56:34 -0500

    Added game functionality and updated state locations

--- US States Quiz/main.py
+++ US States Quiz/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 WIDTH, HEIGHT = 725, 500
 
@@ -9,6 +10,21 @@ image = "Udemy/US States Quiz/blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="Enter a State's name.")
+data = pandas.read_csv("Udemy/US States Quiz/50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+
+    answer_state = screen.textinput(title=f"{len(guessed_states)} /50 States Correct", prompt="Enter a State's name.").title()
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.pu()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
 
 screen.exitonclick()
