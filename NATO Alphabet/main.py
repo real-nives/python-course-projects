83f957bbe2756a2cf93608e8cc58467a31416b63
nives <nickives@live.com>
2025-01-18 20:14:27 -0600

    Added exception handling for KeyError

--- NATO Alphabet/main.py
+++ NATO Alphabet/main.py
@@ -6,6 +6,15 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
 #Take a word as input and print the phonetic letters in the word.
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("You must only enter letters in the alphabet.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
